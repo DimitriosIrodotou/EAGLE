4e086f7113174ff15ee631ac7d7caad7cb05ee0e
DimitriosIrodotou <di.irodotou@gmail.com>
2019-09-04 17:31:23 +0100

    Update position_histogram.py

--- position_histogram.py
+++ position_histogram.py
@@ -1,42 +1,48 @@
+import re
 import time
 
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+from matplotlib import gridspec
 
 import eagle_IO.eagle_IO.eagle_IO as E
 
+sns.set()
+sns.set_style('ticks', {'axes.grid': True})
+sns.set_context('notebook', font_scale=1.6)
 date = time.strftime("%d_%m_%y_%H%M")
+outdir = '/cosma7/data/dp004/dc-irod1/G-EAGLE/python/plots/'
 
 
 class PositionHistogram:
 
-    def __init__(self, tag, sim):
-        start_time = time.time()  # Start the time.
+    def __init__(self, sim, tag):
+        start_global_time = time.time()  # Start the global time.
 
+        start_local_time = time.time()  # Start the local time.
         # Load data #
-        self.stellar_data = self.read_galaxy(tag, sim)
-        print("--- Finished reading the data in %.5s seconds ---" % (time.time() - start_time))  # Print reading time.
-        start_time = time.time()  # Start the time.
+        self.stellar_data = self.read_galaxy(sim, tag)
+        print("--- Finished reading the data in %.5s seconds ---" % (time.time() - start_local_time))  # Print reading time.
 
         # Plot #
         self.plot()
-        print("--- Finished plotting the data in %.5s seconds ---" % (time.time() - start_time))  # Print plotting time.
+        print("--- Finished PositionHistogram.py in %.5s seconds ---" % (time.time() - start_global_time))  # Print plotting time.
 
 
-    def read_galaxy(self, tag, sim):
+    def read_galaxy(self, sim, tag):
         """
          For a given galaxy (defined by its GroupNumber and SubGroupNumber) extract the selected particle and subhalo properties.
 
-        :param tag: redshift
         :param sim: simulation directory
+        :param tag: redshift folder
         :return: subhalo_data, stellar_data
         """
 
         # Load subhalo data #
         subhalo_data = {}
         file_type = 'SUBFIND'
-
         for att in ['GroupNumber', 'SubGroupNumber', 'CentreOfPotential']:
             subhalo_data[att] = E.read_array(file_type, sim, tag, '/Subhalo/' + att, noH=True, physicalUnits=True, CGS=True, numThreads=4,
                                              verbose=False)
@@ -45,10 +51,12 @@ class PositionHistogram:
         stellar_data = {}
         particle_type = '4'
         file_type = 'PARTDATA'
-
         for att in ['GroupNumber', 'SubGroupNumber', 'Coordinates']:
             stellar_data[att] = E.read_array(file_type, sim, tag, '/PartType' + particle_type + '/' + att, noH=True, physicalUnits=True, CGS=True,
                                              numThreads=4, verbose=False)
+
+        # Normalise coordinates wrt the centre of mass of the subhalo #
+        stellar_data['Coordinates'] = np.subtract(stellar_data['Coordinates'], subhalo_data['CentreOfPotential'][0])
 
         # Mask to selected GroupNumber and SubGroupNumber #
         cop = E.read_array('SUBFIND', sim, tag, '/Subhalo/CentreOfPotential', numThreads=4)
@@ -60,7 +68,7 @@ class PositionHistogram:
             stellar_data[att] = stellar_data[att][mask]
 
         # Convert to astronomical units #
-        stellar_data['Coordinates'] *= u.cm.to(u.Mpc)
+        stellar_data['Coordinates'] *= u.cm.to(u.kpc)
 
         return stellar_data
 
@@ -70,29 +78,43 @@ class PositionHistogram:
 
         :return: None
         """
-
+        # Generate the figures #
         plt.close()
-        figure = plt.figure(0, figsize=(10, 7.5))
+        figure = plt.figure(0, figsize=(10, 10))
 
-        plt.xlabel(r'$\mathrm{x/Mpc}$')
-        plt.ylabel(r'$\mathrm{y/Mpc}$')
-        plt.tick_params(direction='in', which='both', top='on', right='on')
+        gs = gridspec.GridSpec(2, 2, height_ratios=[2, 1], width_ratios=(20, 1))
+        gs.update(hspace=0.2)
+        axtop = plt.subplot(gs[0, 0])
+        axbot = plt.subplot(gs[1, 0])
+        axcbar = plt.subplot(gs[:, 1])
 
-        # Plot.
-        hexbin = plt.hexbin(list(zip(*self.stellar_data['Coordinates']))[0], list(zip(*self.stellar_data['Coordinates']))[1], bins='log',
-                            xscale='log', yscale='log', cmap="Greys", gridsize=150, edgecolor='none')
-        # Adjust the color bar #
-        cbaxes = figure.add_axes([0.9, 0.11, 0.02, 0.77])
-        cb = plt.colorbar(hexbin, cax=cbaxes)
-        cb.set_label('$\mathrm{Counts\; per\; hexbin}$')
+        # Generate the XY projection #
+        axtop.set_xlabel(r'$\mathrm{x/kpc}$')
+        axtop.set_ylabel(r'$\mathrm{y/kpc}$')
+        axtop.tick_params(direction='in', which='both', top='on', right='on')
 
-        # Save plot.
-        plt.savefig('PositionHistogram' + '-' + date + '.png', bbox_inches='tight')
+        pltop = axtop.hexbin(list(zip(*self.stellar_data['Coordinates']))[0], list(zip(*self.stellar_data['Coordinates']))[1], bins='log',
+                             cmap="Greys", gridsize=150, edgecolor='none')
+
+        # Generate the XZ projection #
+        axbot.set_xlabel(r'$\mathrm{x/kpc}$')
+        axbot.set_ylabel(r'$\mathrm{z/kpc}$')
+
+        plbot = axbot.hexbin(list(zip(*self.stellar_data['Coordinates']))[0], list(zip(*self.stellar_data['Coordinates']))[2], bins='log',
+                             cmap="Greys", gridsize=150, edgecolor='none')
+
+        # Generate the color bar #
+        cbar = plt.colorbar(pltop, cax=axcbar)
+        cbar.set_label('$\mathrm{log_{10}(Particles\; per\; hexbin)}$')
+
+        # Save plot #
+        plt.title('z ~ ' + re.split('_z0|p000', tag)[1])
+        plt.savefig(outdir + 'PH' + '-' + date + '.png', bbox_inches='tight')
 
         return None
 
 
 if __name__ == '__main__':
-    tag = '005_z010p000'
+    tag = '010_z005p000'
     sim = '/cosma7/data/dp004/dc-payy1/G-EAGLE/GEAGLE_06/data/'
-    x = PositionHistogram(tag, sim)
+    x = PositionHistogram(sim, tag)
